9bd79d1066025a1776f51f95484ccc70739561e6
Alexander Moreno <alexander.gtz2789@gmail.com>
2026-06-05 21:06:14 -0600

    Enhance Pokemon search with additional details

--- practice3/script.js
+++ practice3/script.js
@@ -2,6 +2,12 @@ const pokemonName = document.getElementById("pokemon_name");
 const pokemonImage = document.getElementById("pokemon_image");
 const pokemonType = document.getElementById("pokemon_types");
 const pokemonTitle = document.getElementById("pokemon_title");
+const pokemonId = document.getElementById("pokemon_id");
+const pokemonHeight = document.getElementById("pokemon_height");
+const pokemonWeight = document.getElementById("pokemon_weight");
+const pokemonAbilities = document.getElementById("pokemon_abilities");
+const pokemonStats = document.getElementById("pokemon_stats");
+
 const button = document.getElementById("search_button");
 
 const resultSection = document.getElementById("result_section");
@@ -20,9 +26,14 @@ button.addEventListener("click", async () => {
         }
         const datos = await respuesta.json();
         console.log(datos);
-        pokemonImage.src = datos.sprites.front_default;
+        pokemonImage.src = datos.sprites.front_shiny || datos.sprites.front_default;
+        pokemonId.textContent = "ID: " + datos.id;
         pokemonTitle.textContent = "Name: " + datos.name.charAt(0).toUpperCase() + datos.name.slice(1);
         pokemonType.textContent = "Type: " + datos.types.map(typeInfo => typeInfo.type.name).join(", ");
+        pokemonHeight.textContent = "Height: " + datos.height;
+        pokemonWeight.textContent = "Weight: " + datos.weight;
+        pokemonAbilities.textContent = "Abilities: " + datos.abilities.map(abilityInfo => abilityInfo.ability.name).join(", ");
+        pokemonStats.textContent = "Stats: " + datos.stats.map(statInfo => `${statInfo.stat.name}: ${statInfo.base_stat}`).join(", ");
     }
     catch(error){
         console.error(error);
